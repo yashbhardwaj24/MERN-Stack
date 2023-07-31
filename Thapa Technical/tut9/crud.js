e57f6601319b80b08e054cbf48fde333bb7efa64
yashbhardwaj24 <yash.yyy.24@gmail.com>
2023-07-31 10:23:20 +0530

    CRUD using Async

---
+++ Thapa Technical/tut9/crud.js
@@ -0,0 +1,17 @@
+/* 
+TODO : Node.JS Challenge #2: Asynchronous CRUD Operations using File System Modules in NodeJS in Hindi
+? https://www.youtube.com/watch?v=5E1fqFs2IuQ&list=PLwGdqUZWnOp00IbeN0OtL9dmnasipZ9x8&index=9
+*/
+
+const fs=require("fs");
+
+// fs.mkdir("yash", (err)=> {console.log("Executed");});
+
+// fs.writeFile("./yash/read.txt","Hii how are you Yash", (err)=>{console.log("executed");});
+
+// fs.appendFile("./yash/read.txt","Hello Yash", (err)=> {console.log("executed");});
+
+// fs.rename("./yash/read.txt", "./yash/Mread.txt",(err)=>{console.log("executed");});
+
+// fs.unlink("./yash/Mread.txt",(err)=> {console.log("executed");});
+// fs.rmdir("yash", (err)=> {console.log("executed");});

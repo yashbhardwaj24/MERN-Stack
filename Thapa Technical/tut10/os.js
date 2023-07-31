8b208660d697bae38c5e522399f3d9516649a9a9
yashbhardwaj24 <yash.yyy.24@gmail.com>
2023-07-31 11:43:05 +0530

    os mudolues

---
+++ Thapa Technical/tut10/os.js
@@ -0,0 +1,17 @@
+/*  
+TODO : Node.JS OS Module To Get Operating System Info in Hindi | Node JS Tutorial #10
+? https://www.youtube.com/watch?v=tFKzxizRkfY&list=PLwGdqUZWnOp00IbeN0OtL9dmnasipZ9x8&index=10
+*/
+
+const os =require("os");
+
+// console.log(os.arch());
+
+const osfreemem = os.freemem();
+// console.log(osfreemem);
+// console.log(`${osfreemem/1024/1024/1024}`);
+const tmem = os.totalmem();
+
+// console.log(tmem);
+// console.log(`${tmem/1024/1024/1024}`);
+

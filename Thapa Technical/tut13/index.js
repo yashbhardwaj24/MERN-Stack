663fbc6282f7257c69320455cd7a34231a54bbae
yashbhardwaj24 <yash.yyy.24@gmail.com>
2023-08-11 13:51:46 +0530

    NPM modules

---
+++ Thapa Technical/tut13/index.js
@@ -0,0 +1,15 @@
+/* 
+TODO :  Complete NPM In 20Mintues | Import NPM Module in Node.JS in Hindi in 2020 #13
+?https://www.youtube.com/watch?v=bpssxANRL54&list=PLwGdqUZWnOp00IbeN0OtL9dmnasipZ9x8&index=13
+*/
+
+// Importing chalk module
+import chalk from "chalk";
+
+console.log(chalk.red("Hello"));
+console.log(chalk.bgBlackBright("Hello"));
+console.log(chalk.underline("Hello"));
+
+import validator from "validator";
+
+console.log(validator.isEmail("yash@mail.com"));
